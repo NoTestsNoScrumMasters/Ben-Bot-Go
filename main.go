6aa05716259fd3ae9a5d6a14bcbced7931b2e366
FriendlyTornado <ctrl.vee@gmail.com>
2024-02-02 10:09:35 -0700

    import change

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
-import (
-	"https://github.com/NoTestsNoScrumMasters/Ben-Bot-Go"
-)
+func main() {
+	bot.BotToken = "DISCORD TOKEN ID"
+	bot.Run() // call the run function of bot/bot.go
+}
